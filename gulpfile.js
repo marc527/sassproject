e90375e66d0e41062764421a615ecb3f77aad957
Marc Huang <yu.huang.151@gmail.com>
2017-03-26 14:15:51 +1100

    add gulp compass and html tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 var gulp = require('gulp'),
  concat = require('gulp-concat'),
  browserify = require('gulp-browserify'),
- connect = require('gulp-connect');
+ connect = require('gulp-connect'),
+ compass = require('gulp-compass'),
+ gutil = require('gulp-util'),
+ watch = require('gulp-watch');
 
 var jsSources = ['components/scripts/*.js'];
+var sassSources = ['components/sass/style.scss'];
 
 gulp.task('js', function(){
   gulp.src(jsSources)
@@ -13,6 +17,30 @@ gulp.task('js', function(){
     .pipe(connect.reload())
 });
 
+gulp.task('compass', function(){
+  gulp.src(sassSources)
+  .pipe(compass({
+    css: 'builds/development/css',
+    sass: 'components/sass',
+    image: 'builds/development/images',
+    style: 'expanded'
+  })
+  .on('error', gutil.log))
+  .pipe(gulp.dest('builds/development/css'))
+  .pipe(connect.reload())
+});
+
+gulp.task('html', function(){
+  gulp.src('builds/development/*.html')
+    .pipe(connect.reload())
+});
+
+gulp.task('watch', function(){
+  gulp.watch(jsSources, ['js']);
+  gulp.watch(sassSources, ['compass']);
+  gulp.watch('builds/development/*.html', ['html']);
+});
+
 gulp.task('connect', function() {
   connect.server({
     root: 'builds/development',
@@ -20,4 +48,4 @@ gulp.task('connect', function() {
   });
 });
 
-gulp.task('default', ['js', 'connect']);
+gulp.task('default', ['js', 'compass', 'watch', 'connect']);
